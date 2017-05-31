a7af8b72c974a45f5a2258bc84e20e0f08f56765
flyceratops <rogerdenesha@gmail.com>
2017-05-31 10:05:15 -0500

    Update index.py
    The txpo is intentional - it's ironic, right?

--- index.py
+++ index.py
@@ -2,3 +2,5 @@
 # do not underestimate the power of it
 print "hwllo world"
 # yeah, see what i mean?  your mind just got blown, didn't it?
+# yes, that was me in your neighbor's birdbath but don't tell anyone
+
